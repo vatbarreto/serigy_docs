9cb1e05ea13f4db8adc49607c05954b21690009a
vatbarreto <vin.teles@gmail.com>
2023-02-07 10:01:41 -0300

    Fetch data to hilite.me API for HTML formatting; Replacing <example></example> tags with new formatted HTML;

--- index.js
+++ index.js
@@ -25,7 +25,27 @@ read_html(file_url).then(html => {
     const $ = cheerio.load(html)
     $('example').get().map(
         async example => {
-            console.log($(example).html())
+            // console.log($(example).html())
+            let form_data = new FormData()
+            form_data.append('code', $(example).html())
+            form_data.append('lexer', 'html')
+            form_data.append('options', '')
+            form_data.append('style', 'colorful')
+            form_data.append('linenos', '')
+            form_data.append('divstyles', 'border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;')
+            const response = await fetch(
+                api_url,
+                {
+                    method: 'POST',
+                    body: form_data
+                }
+            )
+            response.text().then(
+                html => {
+                    $(example).replaceWith(html)
+                    write_html(`new_${file_url}`, $.html())
+                }
+            )
         }
     )
 }).catch(error => console.error(error))
