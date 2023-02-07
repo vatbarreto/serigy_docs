d617dcaeda729dd24784fc32ddc989304403d30c
vatbarreto <vin.teles@gmail.com>
2023-02-07 20:44:33 -0300

    Code refactor to log which file has been saved, and replace map loop with a for loop

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 (async () => {
 
-const fs = require('fs')
-const cheerio = require('cheerio')
-const htmlparser2 = require('htmlparser2')
-
-const api_url = 'http://hilite.me/api'
-const input_dir = 'html/input/'
-const output_dir = 'html/output/'
-
-async function list_html() {
-    try {
-        const files = await fs.promises.readdir(input_dir)
-        return files
-    } catch(error) {
-        console.error(error)
+    const fs = require('fs')
+    const cheerio = require('cheerio')
+    const htmlparser2 = require('htmlparser2')
+    
+    const api_url = 'http://hilite.me/api'
+    const input_dir = 'html/input/'
+    const output_dir = 'html/output/'
+    
+    async function list_html() {
+        try {
+            const files = await fs.promises.readdir(input_dir)
+            return files
+        } catch(error) {
+            console.error(error)
+        }
     }
-}
-
-async function read_html(filename) {
-    try {
-        const html_data = await fs.promises.readFile(`${input_dir}${filename}`, 'utf-8')
-        return html_data
-    } catch(error) {
-        console.error(error)
+    
+    async function read_html(filename) {
+        try {
+            const html_data = await fs.promises.readFile(`${input_dir}${filename}`, 'utf-8')
+            return html_data
+        } catch(error) {
+            console.error(error)
+        }
     }
-}
-
-async function write_html(filename, data) {
-    try {
-        await fs.promises.writeFile(`${output_dir}${filename}`, data)
-    } catch(error) {
-        console.error(error)
+    
+    async function write_html(filename, data) {
+        try {
+            await fs.promises.writeFile(`${output_dir}${filename}`, data)
+        } catch(error) {
+            console.error(error)
+        }
     }
-}
-
-async function format_html(filename) {
-    const html_data = await read_html(filename)
-    const document = htmlparser2.parseDocument(html_data)
-    const $ = cheerio.load(document)
-    let i = 1
-    $('example').get().map(
-        async example => {
-            // console.log($(example).html())
+    
+    async function format_html(filename) {
+        const html_data = await read_html(filename)
+        const document = htmlparser2.parseDocument(html_data)
+        const $ = cheerio.load(document)
+        let i = 1
+        for (let example of $('example').get()) {
             let form_data = new FormData()
             form_data.append('code', $(example).html())
             form_data.append('lexer', 'html')
@@ -60,19 +58,22 @@ async function format_html(filename) {
             if (response.ok) {
                 process.stdout.write(`### Formatting ${filename} example tag #${i++}... `)
                 // console._stdout.write(`### Formatting ${filename} example tag #${i++}... `)
-                // console.log($(example).html())
                 html_text = await response.text()
                 $(example).replaceWith(html_text)
-                write_html(filename, $.html().replaceAll('<br></br>', '<br />'))
-                console.log('Finished!')
+                console.log('ok!')
             } else {
                 console.error('### API Response Error! ###')
             }
         }
-    )
-}
-
-const files = await list_html()
-files.map(async file => await format_html(file))
-
-})()
+        process.stdout.write(`>>> Saving file ${filename}... `)
+        const html = $.html()
+                      .replaceAll('<!-- HTML generated using hilite.me -->', '')
+                      .replaceAll('<br></br>', '<br />')
+        write_html(filename, html)
+        console.log('Finished!')
+    }
+    
+    const files = await list_html()
+    files.map(async file => await format_html(file))
+    
+    })()
