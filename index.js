b7dab8a82e5a7f662496d81eed15164839d2d63a
vatbarreto <vin.teles@gmail.com>
2023-02-08 14:58:39 -0300

    Code refactor for performance and readability improvements

--- index.js
+++ index.js
@@ -35,47 +35,58 @@
     }
 
     async function request_api(html) {
-        let form_data = new FormData()
-        form_data.append('code', html)
-        form_data.append('lexer', 'html')
-        form_data.append('options', '')
-        form_data.append('style', 'autumn')
-        form_data.append('linenos', '')
-        form_data.append('divstyles', 'border:solid #0072c6;border-width:.1em .1em .1em .8em;padding:.2em .6em;')
-        // form_data.append('divstyles', 'border:solid #008f05;border-width:.1em .1em .1em .8em;padding:.2em .6em;')
-        const response = await fetch(
-            api_url, {
-                method: 'POST',
-                body: form_data
+        try {
+            let form_data = new FormData()
+            form_data.append('code', html)
+            form_data.append('lexer', 'html')
+            form_data.append('options', '')
+            form_data.append('style', 'autumn')
+            form_data.append('linenos', '')
+            form_data.append('divstyles', 'border:solid #0072c6;border-width:.1em .1em .1em .8em;padding:.2em .6em;')
+            // form_data.append('divstyles', 'border:solid #008f05;border-width:.1em .1em .1em .8em;padding:.2em .6em;')
+            const response = await fetch(
+                api_url, {
+                    method: 'POST',
+                    body: form_data
+                }
+            )
+            if (!response.ok) {
+                console.error(`### ERROR: API Response status code ${response.status}`)
+                return null
             }
-        )
-        if (!response.ok) {
-            console.error('### API Response Error! ###')
+            return response.text()
+        } catch(error) {
+            console.error(`### ERROR: API request returned error ${error}`)
             return null
         }
-        return response.text()
     }
     
     async function format_html(filename) {
         const html_data = await read_html(filename)
         const document = htmlparser2.parseDocument(html_data)
         const $ = cheerio.load(document)
-        Promise.all( $('example').get().map( async (example, i) => {
-            const formatted_example = await request_api($(example).html())
-            process.stdout.write(`### Formatting ${filename} example tag #${parseInt(i)+1}... `)
-            $(example).replaceWith(formatted_example)
-            console.log('ok!')
-        } ) ).then( () => {
-            process.stdout.write(`>>> Saving file ${filename}... `)
-            const html = $.html()
-                          .replaceAll('<!-- HTML generated using hilite.me -->', '')
-                          .replaceAll('<br></br>', '<br />')
-            write_html(filename, html)
-            console.log('Finished!')
-        } )
+        await Promise.all($('example').get().map(
+            async (original_example, i) => {
+                try {
+                    const formatted_example = await request_api($(original_example).html())
+                    process.stdout.write(`# Formatting '${filename}' example tag #${parseInt(i)+1}... `)
+                    $(original_example).replaceWith(formatted_example)
+                    console.log('ok!')
+                } catch(error) {
+                    console.error(error)
+                    return null
+                }
+            }
+        ))
+        process.stdout.write(`>>> Saving file '${filename}'... `)
+        const html = $.html()
+                      .replaceAll('<!-- HTML generated using hilite.me -->', '')
+                      .replaceAll('<br></br>', '<br />')
+        await write_html(filename, html)
+        console.log('Finished!')
     }
     
     const files = await list_html()
-    files.map(async file => await format_html(file))
+    files.forEach(async file => await format_html(file))
     
 })()
